9f728d31e20dd33d5f0cf2911afe13b4977a165f
NazuNazuzu <luxeno103@gmail.com>
2026-08-24 21:54:26 +0900

    feat: add REC_NOTIFY

---
+++ client/modules/notify/rec.lua
@@ -0,0 +1,31 @@
+
+---@type REC_Library.Shared.API
+local shApi = require "@REC_Library.shared.sh_api"
+local apiShCfg = shApi.Config
+local notify = apiShCfg.notify
+
+local apiShEnums = shApi.Enums
+local notifyTypes = apiShEnums.NotifyTypes
+
+if notify ~= notifyTypes.rec then
+    return
+end
+
+local recNotify = exports.REC_Notify
+
+---@type REC_Utils.Client.Modules.Notify
+---@diagnostic disable-next-line: missing-fields
+local REC_NOTIFY = {}
+
+function REC_NOTIFY:trigger(notifyType, title, msg, duration, playSound)
+    recNotify:notify({
+        type = notifyType,
+        title = title,
+        description = msg,
+        duration = duration,
+        playSound = playSound,
+    })
+    return true
+end
+
+return REC_NOTIFY
---
+++ server/modules/notify/rec.lua
@@ -0,0 +1,31 @@
+
+---@type REC_Library.Shared.API
+local shApi = require "@REC_Library.shared.sh_api"
+local apiShCfg = shApi.Config
+local notify = apiShCfg.notify
+
+local apiShEnums = shApi.Enums
+local notifyTypes = apiShEnums.NotifyTypes
+
+if notify ~= notifyTypes.rec then
+    return
+end
+
+local recNotify = exports.REC_Notify
+
+---@type REC_Utils.Server.Modules.Notify
+---@diagnostic disable-next-line: missing-fields
+local REC_NOTIFY = {}
+
+function REC_NOTIFY:trigger(playerId, notifyType, title, msg, duration, playSound)
+    recNotify:notify(playerId, {
+        type = notifyType,
+        title = title,
+        description = msg,
+        duration = duration,
+        playSound = playSound,
+    })
+    return true
+end
+
+return REC_NOTIFY
